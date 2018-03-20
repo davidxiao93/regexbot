aa68b39a283075b2ea7ed31961d341ac69a1c18b
David Xiao <davidxiao93@gmail.com>
2018-03-20 20:33:43 +0000

    Use list instead of dictionary

--- regexbot.py
+++ regexbot.py
@@ -6,6 +6,7 @@ from sheetclient import SheetClient
 
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
+import random
 
 if "SHEET_ID" not in os.environ:
     print("Run \"export SHEET_ID = \'<your google sheet Id>\'\" first")
@@ -26,31 +27,43 @@ slack_client = SlackClient(slack_bot_token)
 starterbot_id = None
 
 RTM_READ_DELAY = 0.01 # 0.1 second delay between reading from RTM
-MAX_LENGTH = 256
+MAX_LENGTH = 128
+MAX_NUMBER_OF_REGEXES = 100
 
-compiled_regex_dict = {}
+compiled_regex_list = []
 
 def load_regexes():
+    global compiled_regex_list
+    compiled_regex_list = []
     sheet_client.clear_status()
     raw_regex_list = sheet_client.get_regexes()
+    regex_dict = {}
+    compiled_regex_count = 0
     for i, row in enumerate(raw_regex_list):
         sheet_client.update_status(i + 2, "Checking")
         message = "Accepted"
+        if compiled_regex_count > MAX_NUMBER_OF_REGEXES:
+            message = "Too many regexes in sheet"
         if len(row) >= 2 and len(row[0].strip()) == 0 and len(row[1].strip()) == 0:
             message = ""
         elif len(row) < 2 or len(row[0].strip()) == 0 or len(row[1].strip()) == 0:
             message = "Empty Cell"
         elif len(row[0].strip()) > MAX_LENGTH or len(row[1].strip()) > MAX_LENGTH:
-            print(len(row[0].strip()), len(row[1].strip()))
             message = "Regex too long: " + str(len(row[0].strip())) + ", " + str(len(row[1].strip())) + ", maximum is " + str(MAX_LENGTH)
         else:
             source_regex = row[0].strip()
             destination_regex = row[1].strip()
             try:
-                compiled_regex_dict[re.compile(source_regex)] = destination_regex
+                compiled_regex = re.compile(source_regex)
+                if compiled_regex not in regex_dict:
+                    regex_dict[compiled_regex] = []
+                    compiled_regex_count += 1
+                regex_dict[compiled_regex].append(destination_regex)
             except re.error as e:
-                message = "Error"
+                message = "Error compiling regex"
         sheet_client.update_status(i+2, message)
+    for compiled_regex, destination_regexes in regex_dict.items():
+        compiled_regex_list.append((compiled_regex, destination_regexes))
 
 def handle_message(slack_event):
     message_text = slack_event["text"]
@@ -60,10 +73,10 @@ def handle_message(slack_event):
         load_regexes()
         return
 
-    for source_regex, destination_regex in compiled_regex_dict.items():
+    for source_regex, destination_regexes in compiled_regex_list:
         try:
             if source_regex.search(message_text):
-                new_message = re.sub(source_regex, destination_regex, message_text)
+                new_message = re.sub(source_regex, random.choice(destination_regexes), message_text)
 
                 slack_client.api_call(
                     "chat.postMessage",
